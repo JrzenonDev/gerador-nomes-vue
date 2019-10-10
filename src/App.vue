3a471629a6b015290413658f0be118dc59c426e6
JrzenonDev <jrobertoonb@gmail.com>
2019-10-10 02:28:05 -0300

    arquivos inicias, com estrutura default de inicio de projeto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="slogan">
+    <div class="text-center">
+      <h1>Name Gator</h1>
+      <br>
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+  #slogan {
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+</style>
