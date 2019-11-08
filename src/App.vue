4a82f298e03f1a096d681bddd0e7147358d0dd9f
JrzenonDev <jrobertoonb@gmail.com>
2019-11-07 21:03:06 -0300

    fazendo checkout do domínio já com link de finalização de compra

--- src/App.vue
+++ src/App.vue
@@ -76,8 +76,17 @@
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
-                {{ domain }}
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <div class="row">
+                  <div class="col-md">
+                    {{ domain.name }}
+                  </div>
+                  <div class="col-md text-right">
+                    <a v-bind:href="domain.checkout" target="_blank" class="btn btn-info">
+                      <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
@@ -125,7 +134,13 @@ export default {
 			const domains = [];
 			for (const prefix of this.prefixes) {
 				for (const sufix of this.sufixes) {
-					domains.push(sufix + prefix);
+					const name = prefix + sufix;
+					const url = name.toLowerCase();
+					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+					domains.push({
+						name,
+						checkout
+					});
 				}
 			}
 			return domains;
