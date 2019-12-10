676b515b2884a7e90122e27cd9a428f3696831e8
JrzenonDev <jrobertoonb@gmail.com>
2019-12-09 23:31:31 -0300

    botão de navegação para ver os detalhes do domain item

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -30,6 +30,10 @@
                     <a v-bind:href="domain.checkout" target="_blank" class="btn btn-info">
                       <span class="fa fa-shopping-cart"></span>
                     </a>
+                    &nbsp;
+                    <button class="btn btn-info" @click="openDomain(domain)">
+                      <span class="fa fa-search"></span>
+                    </button>
                   </div>
                 </div>
               </li>
@@ -147,6 +151,11 @@ export default {
 			}).then((response) => {
 				const query = response.data;
 				this.domains = query.data.domains;
+			});
+		},
+		openDomain(domain) {
+			this.$router.push({
+				path: `/domains/${domain.name}`
 			});
 		}
 	},
